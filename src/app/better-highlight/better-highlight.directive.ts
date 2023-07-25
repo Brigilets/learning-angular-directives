141f2bad4684832040ec9a150a786045f2aaf408
Brigilets <b.sabutyte@gmail.com>
2023-07-25 15:04:08 +0200

    replaced code in ngOnInit with @HostListener directive

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,15 +1,37 @@
-import { Directive, OnInit, Renderer2, ElementRef } from '@angular/core';
+import {
+  Directive,
+  OnInit,
+  Renderer2,
+  ElementRef,
+  HostListener,
+} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]',
 })
 export class BetterHighlightDirective implements OnInit {
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+
   ngOnInit(): void {
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'background-color',
+    //   'rgb(215, 203, 164)'
+    // );
+  }
+
+  @HostListener('mouseenter') mouseOver(eventData: Event) {
     this.renderer.setStyle(
       this.elRef.nativeElement,
       'background-color',
       'rgb(215, 203, 164)'
+    );
+  }
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      'background-color',
+      '#fff'
     );
   }
 }
