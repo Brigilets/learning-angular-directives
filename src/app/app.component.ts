e397796a4e5f938fbd741d3ce50c4d66c2b20a33
Brigilets <b.sabutyte@gmail.com>
2023-07-25 10:37:08 +0200

    adding toggle functionality between oddand even numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   numbers = [1, 2, 3, 4, 5];
+  oddNumbers = this.numbers.filter((num) => num % 2 !== 0);
+  evenNumbers = this.numbers.filter((num) => num % 2 === 0);
   onlyOdd = false;
+  numberType: string = 'odd' || 'even';
+
+  toggleNumberType() {
+    this.onlyOdd = !this.onlyOdd;
+    this.numberType = this.onlyOdd ? 'even' : 'odd';
+  }
 }
